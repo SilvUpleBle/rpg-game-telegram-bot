6b7c5c1bccd496b633252e296fb221c8030e05da
SlavaNoBugs <zagvozdkin12@gmail.com>
2023-04-21 11:49:39 +0300

    full commit

--- src/main/java/io/project/TestBot/model/Task_table.java
+++ src/main/java/io/project/TestBot/model/Task_table.java
@@ -1,7 +1,15 @@
 package io.project.TestBot.model;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface Task_table extends CrudRepository<TaskSQL, Long> {
+    TaskSQL findByTaskName(String taskName);
 
+    boolean existsByTaskName(String taskName);
+
+    List<TaskSQL> findAllByCreatorId(Long creatorId);
+
+    List<TaskSQL> findAllByRecipientId(String recipientId);
 }
